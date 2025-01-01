7b9984a54db6dca1ea600d7dd859d4e2d97b32d6
shirlauterbach <shir.lauterbach@gmail.com>
2025-01-01 20:06:55 +0200

    remove dist

--- dist/entities/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Song } from './Song';
-import { Word } from './Word';
-import { UniqueWord } from './UniqueWord';
-import { GroupOfWords } from './GroupOfWords';
-import { Expression } from './Expression';
-export { Song, Word, UniqueWord, GroupOfWords, Expression };
